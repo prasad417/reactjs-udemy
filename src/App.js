18b1eda58f1aa49e6a83ef43faaadd3f6db297b7
prasad <417.prasad@gmail.com>
2019-11-02 00:18:45 -0500

    Manipulate state with class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>React JS App.</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: [
+        { name: "Prasad", age: "33" },
+        { name: "Ramya", age: "27" },
+        { name: "Prayan", age: "3" }
+    ]
+  }
+
+  changeName = () => {
+    console.log('Clicked.')
+    this.setState({
+      person: [
+        { name: "Prasad", age: "32" },
+        { name: "Ramya", age: "27" },
+        { name: "Prayan", age: "2" }
+    ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.changeName}>Switch Names</button>
+        <Person name = {this.state.person[0].name} age = {this.state.person[0].age}>My Hobbies: Long drive</Person>
+        <Person name = {this.state.person[1].name} age = {this.state.person[1].age} />
+        <Person name = {this.state.person[2].name} age = {this.state.person[2].age} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
