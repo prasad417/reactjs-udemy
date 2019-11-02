08f19aef417cdc4c111e7498af4dbd7d3155e588
prasad <417.prasad@gmail.com>
2019-11-02 03:07:24 -0500

    Assignment completion

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react'
 import './App.css'
 import PersonClass from './Person/PersonClassComp'
 import PersonFunction from './Person/PersonFunctionComp'
+import UserInput from './assignments/state/UserInput'
 
 class App extends Component {
+  
   
 
   render() {
     return (
       <div className="App">
-        <PersonClass />
-        
-        <PersonFunction />
+        {/* <PersonClass />
+        <PersonFunction /> */}
+        <UserInput />
       </div>
     );
   }
